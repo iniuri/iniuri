7d23fa63502f625095c151e7482696025d660506
Илья <temnenko078@gmail.com>
2023-05-19 15:31:57 +0200

    balls.

---
+++ lesson2/h2.py
@@ -0,0 +1,27 @@
+import random
+# woohoo import random i surely hope i dont roll snake eyes!
+# edit: rolled snake eyes
+number = random.randint(1, 3)
+# i hate this question
+print(f'--------------------------------------')
+name = input("Im sorry to interrupt but what is your name?\n")
+print(f'--------------------------------------')
+
+class meow:
+    print(f"[ debug | rolling dice ]")
+    if number == 1:
+        print(f"[ debug | rolled 1, snake eyes! ]")
+        print("Meow! - i am a cat, but i imitate just like a human!")
+        print(f"dog - hungry")
+    if number == 2:
+        print(f"[ debug | rolled 2, atleast not 1.... ]")
+        print(f"Meow! - hello human! Your name is: {name}, i know everything.")
+        print(f"dog - (wants to be petted)")
+        print(f'--------------------------------------')
+
+    if number == 3:
+        print(f"[ debug | lucky! rolled 3 ]")
+        print(f"Meow! - Lucky you,  {name}, i am a friendly cat.")
+        print(f"dog - [ gibberish ]")
+        print(f'--------------------------------------')
+# may allah forgive me for trash code
